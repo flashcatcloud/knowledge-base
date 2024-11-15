5b90d3774dfba737018696936eb54af9a4fb012e
niuweili <957905827@qq.com>
2024-11-15 15:01:54 +0800

    feat: 支持npm包

---
+++ index.js
@@ -0,0 +1,71 @@
+const CustomAlert = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.0 自定义告警事件集成指引');
+const Email = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.1 邮件 Email 集成指引');
+const N9e = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.2 夜莺 Flashcat 集成指引.md');
+const Prometheus = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.3 Prometheus 集成指引.md');
+const Grafana = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.4 Grafana 集成指引.md');
+const Zabbix = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.5 Zabbix 集成指引.md');
+const UptimeKuma = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.6 Uptime Kuma 集成指引.md');
+const AliyunARMS = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.7 阿里云 ARMS 集成指引.md');
+const AliyunCmEvent = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.8 阿里云监控 CM 事件集成指引.md');
+const AliyunCm = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.9 阿里云监控 CM 指标集成指引.md');
+const AliyunSLS = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.10 阿里云 SLS 集成指引.md');
+const AWSCW = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.11 AWS CloudWatch 集成指引.md');
+const AzureMonitor = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.12 Azure Monitor 集成指引.md');
+const BaiDuBCM = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.13 百度云监控 BCM 集成指引.md');
+const HuaWeiCES = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.14 华为云监控 CES 集成指引.md');
+const InfluxDB = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.15 Influxdata 集成指引.md');
+const OpenFalcon = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.16 Open Falcon 集成指引.md');
+const PagerDuty = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.17 Pagerduty 集成指引.md');
+const TencentBK = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.18 蓝鲸智云集成指引.md');
+const TencentCLS = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.19 腾讯云 CLS 集成指引.md');
+const TencentCm = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.20 腾讯云监控 CM 集成指引.md');
+const TencentEb = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.21 腾讯云 Event Bridge 集成指引.md');
+const OceanBase = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.22 OceanBase告警事件集成.md');
+const Graylog = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.23 Graylog.md');
+const Skywalking = require('./flashduty/8. 集成引导/8.1 告警集成/8.1.24 Skywalking 告警集成指引.md');
+const Sentry = require('./flashduty/8. 集成引导/8.1.25 Sentry 告警集成指引.md');
+
+const CustomChange = require('./flashduty/8. 集成引导/8.2 变更集成/8.2.1 自定义事件集成指引.md');
+const Jira = require('./flashduty/8. 集成引导/8.2 变更集成/8.2.2 Jira 事件集成指引.md');
+
+const Lark = require('./flashduty/8. 集成引导/8.3 即时消息/8.3.1 飞书 Lark 集成指引.md');
+const Dingtalk = require('./flashduty/8. 集成引导/8.3 即时消息/8.3.2 钉钉 Dingtalk 集成指引.md');
+const Wecom = require('./flashduty/8. 集成引导/8.3 即时消息/8.3.3 企业微信 Wecom 集成指引.md');
+const Slack = require('./flashduty/8. 集成引导/8.3 即时消息/8.3.4 Slack 集成指引.md');
+const MicrosoftTeams = require('./flashduty/8. 集成引导/8.3 即时消息/8.3.5 Microsoft Teams 集成指引.md');
+
+module.exports = {
+  CustomAlert,
+  Email,
+  N9e,
+  Prometheus,
+  Grafana,
+  Zabbix,
+  UptimeKuma,
+  AliyunARMS,
+  AliyunCmEvent,
+  AliyunCm,
+  AliyunSLS,
+  AWSCW,
+  AzureMonitor,
+  BaiDuBCM,
+  HuaWeiCES,
+  InfluxDB,
+  OpenFalcon,
+  PagerDuty,
+  TencentBK,
+  TencentCLS,
+  TencentCm,
+  TencentEb,
+  OceanBase,
+  Graylog,
+  Skywalking,
+  Sentry,
+  CustomChange,
+  Jira,
+  Lark,
+  Dingtalk,
+  Wecom,
+  Slack,
+  MicrosoftTeams,
+};
---
+++ index.mjs
@@ -0,0 +1,71 @@
+import CustomAlert from "./flashduty/8. 集成引导/8.1 告警集成/8.1.0 自定义告警事件集成指引";
+import Email from "./flashduty/8. 集成引导/8.1 告警集成/8.1.1 邮件 Email 集成指引";
+import N9e from "./flashduty/8. 集成引导/8.1 告警集成/8.1.2 夜莺 Flashcat 集成指引.md";
+import Prometheus from "./flashduty/8. 集成引导/8.1 告警集成/8.1.3 Prometheus 集成指引.md";
+import Grafana from "./flashduty/8. 集成引导/8.1 告警集成/8.1.4 Grafana 集成指引.md";
+import Zabbix from "./flashduty/8. 集成引导/8.1 告警集成/8.1.5 Zabbix 集成指引.md";
+import UptimeKuma from "./flashduty/8. 集成引导/8.1 告警集成/8.1.6 Uptime Kuma 集成指引.md";
+import AliyunARMS from "./flashduty/8. 集成引导/8.1 告警集成/8.1.7 阿里云 ARMS 集成指引.md";
+import AliyunCmEvent from "./flashduty/8. 集成引导/8.1 告警集成/8.1.8 阿里云监控 CM 事件集成指引.md";
+import AliyunCm from "./flashduty/8. 集成引导/8.1 告警集成/8.1.9 阿里云监控 CM 指标集成指引.md";
+import AliyunSLS from "./flashduty/8. 集成引导/8.1 告警集成/8.1.10 阿里云 SLS 集成指引.md";
+import AWSCW from "./flashduty/8. 集成引导/8.1 告警集成/8.1.11 AWS CloudWatch 集成指引.md";
+import AzureMonitor from "./flashduty/8. 集成引导/8.1 告警集成/8.1.12 Azure Monitor 集成指引.md";
+import BaiDuBCM from "./flashduty/8. 集成引导/8.1 告警集成/8.1.13 百度云监控 BCM 集成指引.md";
+import HuaWeiCES from "./flashduty/8. 集成引导/8.1 告警集成/8.1.14 华为云监控 CES 集成指引.md";
+import InfluxDB from "./flashduty/8. 集成引导/8.1 告警集成/8.1.15 Influxdata 集成指引.md";
+import OpenFalcon from "./flashduty/8. 集成引导/8.1 告警集成/8.1.16 Open Falcon 集成指引.md";
+import PagerDuty from "./flashduty/8. 集成引导/8.1 告警集成/8.1.17 Pagerduty 集成指引.md";
+import TencentBK from "./flashduty/8. 集成引导/8.1 告警集成/8.1.18 蓝鲸智云集成指引.md";
+import TencentCLS from "./flashduty/8. 集成引导/8.1 告警集成/8.1.19 腾讯云 CLS 集成指引.md";
+import TencentCm from "./flashduty/8. 集成引导/8.1 告警集成/8.1.20 腾讯云监控 CM 集成指引.md";
+import TencentEb from "./flashduty/8. 集成引导/8.1 告警集成/8.1.21 腾讯云 Event Bridge 集成指引.md";
+import OceanBase from "./flashduty/8. 集成引导/8.1 告警集成/8.1.22 OceanBase告警事件集成.md";
+import Graylog from "./flashduty/8. 集成引导/8.1 告警集成/8.1.23 Graylog.md";
+import Skywalking from "./flashduty/8. 集成引导/8.1 告警集成/8.1.24 Skywalking 告警集成指引.md";
+import Sentry from "./flashduty/8. 集成引导/8.1.25 Sentry 告警集成指引.md";
+
+import CustomChange from "./flashduty/8. 集成引导/8.2 变更集成/8.2.1 自定义事件集成指引.md";
+import Jira from "./flashduty/8. 集成引导/8.2 变更集成/8.2.2 Jira 事件集成指引.md";
+
+import Lark from "./flashduty/8. 集成引导/8.3 即时消息/8.3.1 飞书 Lark 集成指引.md";
+import Dingtalk from "./flashduty/8. 集成引导/8.3 即时消息/8.3.2 钉钉 Dingtalk 集成指引.md";
+import Wecom from "./flashduty/8. 集成引导/8.3 即时消息/8.3.3 企业微信 Wecom 集成指引.md";
+import Slack from "./flashduty/8. 集成引导/8.3 即时消息/8.3.4 Slack 集成指引.md";
+import MicrosoftTeams from "./flashduty/8. 集成引导/8.3 即时消息/8.3.5 Microsoft Teams 集成指引.md";
+
+export {
+  CustomAlert,
+  Email,
+  N9e,
+  Prometheus,
+  Grafana,
+  Zabbix,
+  UptimeKuma,
+  AliyunARMS,
+  AliyunCmEvent,
+  AliyunCm,
+  AliyunSLS,
+  AWSCW,
+  AzureMonitor,
+  BaiDuBCM,
+  HuaWeiCES,
+  InfluxDB,
+  OpenFalcon,
+  PagerDuty,
+  TencentBK,
+  TencentCLS,
+  TencentCm,
+  TencentEb,
+  OceanBase,
+  Graylog,
+  Skywalking,
+  Sentry,
+  CustomChange,
+  Jira,
+  Lark,
+  Dingtalk,
+  Wecom,
+  Slack,
+  MicrosoftTeams,
+};
