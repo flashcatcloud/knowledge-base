93290648e1da57fbe2df52c13d4714a3abff43d7
niuweili <957905827@qq.com>
2025-05-15 15:50:48 +0800

    update .gitignore

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,38 +9,38 @@ import {
 } from "./chunk-LW4I4DCF.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default5 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
-import { default as default6 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default7 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default9 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default10 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default11 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default12 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
-import { default as default13 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default14 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default15 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default16 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
-import { default as default17 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default18 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default19 } from "/Users/fiona/workspace/flashcat/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default5 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
+import { default as default6 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default7 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default9 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default10 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default11 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default12 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
+import { default as default13 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default14 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default15 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default16 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import { default as default17 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default18 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default19 } from "/Users/niuweili/Desktop/my-project/knowledge-base/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
 
 // node_modules/vitepress/dist/client/theme-default/composables/local-nav.js
 import { onContentUpdated } from "vitepress";
